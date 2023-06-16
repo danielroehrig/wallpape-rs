42c6ec628850102d8ac96294283fcaccafb377dd
Daniel Röhrig <mail@danielroehrig.de>
2023-06-16 14:50:07 +0200

    Cli commands for width an height

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,48 @@
 use raqote::*;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    #[arg(long, value_name ="width", help = "Width of the resulting wallpaper")]
+    width: Option<i32>,
+
+    #[arg(long, value_name ="height", help = "Height of the resulting wallpaper")]
+    height: Option<i32>
+}
 
 fn main() {
-    let mut dt = DrawTarget::new(400, 400);
+    let cli = Args::parse();
+
+    let width = cli.width.unwrap_or(1920);
+    let height = cli.height.unwrap_or(1080);
+
+    let mut dt = DrawTarget::new(width, height);
 
     let mut pb = PathBuilder::new();
-    pb.move_to(100., 10.);
-    pb.cubic_to(150., 40., 175., 0., 200., 10.);
-    pb.quad_to(120., 100., 80., 200.);
-    pb.quad_to(150., 180., 300., 300.);
-    pb.close();
+    pb.rect(0., 0., dt.width() as f32, dt.height() as f32);
     let path = pb.finish();
 
-    let gradient = Source::new_radial_gradient(
+    let gradient = Source::new_linear_gradient(
         Gradient {
             stops: vec![
                 GradientStop {
-                    position: 0.2,
-                    color: Color::new(0xff, 0, 0xff, 0),
+                    position: 0.0,
+                    color: (
+                        Color::new(0xff, 0xcc, 0x00, 0x00)
+                        ),
                 },
                 GradientStop {
-                    position: 0.8,
-                    color: Color::new(0xff, 0xff, 0xff, 0xff),
-                },
-                GradientStop {
-                    position: 1.,
-                    color: Color::new(0xff, 0xff, 0, 0xff),
-                },
-            ],
-        },
-        Point::new(150., 150.),
-        128.,
-        Spread::Pad,
-    );
+                    position: 1.0,
+                    color: (
+                        Color::new(0xff, 0xcc, 0xcc, 0xff)
+                        ),
+                }
+            ]
+        }, Point::new(0., 0.),
+        Point::new(dt.width() as f32, dt.height() as f32), Spread::Pad);
     dt.fill(&path, &gradient, &DrawOptions::new());
 
-    let mut pb = PathBuilder::new();
-    pb.move_to(100., 100.);
-    pb.line_to(300., 300.);
-    pb.line_to(200., 300.);
-    let path = pb.finish();
-
-    dt.stroke(
-        &path,
-        &Source::Solid(SolidSource {
-            r: 0x0,
-            g: 0x0,
-            b: 0x80,
-            a: 0x80,
-        }),
-        &StrokeStyle {
-            cap: LineCap::Square,
-            join: LineJoin::Round,
-            width: 10.,
-            miter_limit: 2.,
-            dash_array: vec![10., 18.],
-            dash_offset: 16.,
-        },
-        &DrawOptions::new(),
-    );
 
     dt.write_png("example.png").unwrap();
 }
