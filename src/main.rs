1202a46f8f60e3e761a3c0ef081378b5db3be25c
Daniel Röhrig <mail@danielroehrig.de>
2023-06-29 11:20:53 +0200

    Use RustFMT

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-use raqote::*;
 use clap::{Parser, Subcommand};
+use rand::seq::SliceRandom;
+use raqote::*;
 use std::collections::HashMap;
 use std::process::exit;
-use rand::seq::SliceRandom;
 
 #[derive(Parser)]
 #[command(version, about)]
 struct Args {
-
     dest: String,
 
     #[arg(long, value_name = "width", help = "Width of the resulting wallpaper")]
     width: Option<i32>,
 
-    #[arg(long, value_name = "height", help = "Height of the resulting wallpaper")]
+    #[arg(
+        long,
+        value_name = "height",
+        help = "Height of the resulting wallpaper"
+    )]
     height: Option<i32>,
 
     #[arg(long, value_name = "palette", help = "Colorscheme to use")]
@@ -26,9 +29,7 @@ struct Args {
 #[derive(Subcommand)]
 enum Commands {
     /// List options
-    List {
-        list: String
-    }
+    List { list: String },
 }
 
 fn main() {
@@ -54,7 +55,6 @@ fn main() {
     let color_scheme = cli.palette.unwrap_or(String::from("cyberpunk"));
     let dest = cli.dest;
 
-
     let palette = match palettes.get(color_scheme.as_str()) {
         Some(x) => x,
         None => {
@@ -67,12 +67,11 @@ fn main() {
 
     draw_gradient(palette, &mut dt);
 
-
     match dt.write_png(dest.clone()) {
         Ok(_) => {
             println!("Image written to {}", dest);
             exit(0);
-        },
+        }
         Err(e) => {
             eprintln!("Could not write to file \"{}\"! Error: {}", dest, e);
             exit(1);
@@ -88,38 +87,44 @@ fn print_palettes(palettes: &HashMap<&str, Vec<Color>>) {
 }
 
 fn build_palettes() -> HashMap<&'static str, Vec<Color>> {
-    HashMap::from(
-        [
-            ("cyberpunk",
-             vec![
-                 Color::new(0xff, 247, 37, 133),
-                 Color::new(0xff, 114, 9, 183),
-                 Color::new(0xff, 58, 12, 163),
-                 Color::new(0xff, 67, 97, 238),
-                 Color::new(0xff, 76, 201, 240),
-             ]),
-            ("pastel",
-             vec![
-                 Color::new(0xff, 205, 180, 219),
-                 Color::new(0xff, 255, 200, 221),
-                 Color::new(0xff, 255, 175, 204),
-                 Color::new(0xff, 189, 224, 254),
-                 Color::new(0xff, 162, 210, 255),
-             ]),
-            ("gundam",
-             vec![
-                 Color::new(0xff, 43, 45, 66),
-                 Color::new(0xff, 141, 153, 174),
-                 Color::new(0xff, 237, 242, 244),
-                 Color::new(0xff, 239, 35, 60),
-                 Color::new(0xff, 217, 4, 41),
-             ])
-        ]
-    )
+    HashMap::from([
+        (
+            "cyberpunk",
+            vec![
+                Color::new(0xff, 247, 37, 133),
+                Color::new(0xff, 114, 9, 183),
+                Color::new(0xff, 58, 12, 163),
+                Color::new(0xff, 67, 97, 238),
+                Color::new(0xff, 76, 201, 240),
+            ],
+        ),
+        (
+            "pastel",
+            vec![
+                Color::new(0xff, 205, 180, 219),
+                Color::new(0xff, 255, 200, 221),
+                Color::new(0xff, 255, 175, 204),
+                Color::new(0xff, 189, 224, 254),
+                Color::new(0xff, 162, 210, 255),
+            ],
+        ),
+        (
+            "gundam",
+            vec![
+                Color::new(0xff, 43, 45, 66),
+                Color::new(0xff, 141, 153, 174),
+                Color::new(0xff, 237, 242, 244),
+                Color::new(0xff, 239, 35, 60),
+                Color::new(0xff, 217, 4, 41),
+            ],
+        ),
+    ])
 }
 
 fn draw_gradient(palette: &Vec<Color>, dt: &mut DrawTarget) {
-    let colors: Vec<&Color> = palette.choose_multiple(&mut rand::thread_rng(), 2).collect();
+    let colors: Vec<&Color> = palette
+        .choose_multiple(&mut rand::thread_rng(), 2)
+        .collect();
 
     let mut pb = PathBuilder::new();
     pb.rect(0., 0., dt.width() as f32, dt.height() as f32);
@@ -130,18 +135,17 @@ fn draw_gradient(palette: &Vec<Color>, dt: &mut DrawTarget) {
             stops: vec![
                 GradientStop {
                     position: 0.0,
-                    color: **(
-                        colors.get(0).unwrap()
-                    ),
+                    color: **(colors.get(0).unwrap()),
                 },
                 GradientStop {
                     position: 1.0,
-                    color: **(
-                        colors.get(1).unwrap()
-                    ),
+                    color: **(colors.get(1).unwrap()),
                 },
-            ]
-        }, Point::new(0., 0.),
-        Point::new(dt.width() as f32, dt.height() as f32), Spread::Pad);
+            ],
+        },
+        Point::new(0., 0.),
+        Point::new(dt.width() as f32, dt.height() as f32),
+        Spread::Pad,
+    );
     dt.fill(&path, &gradient, &DrawOptions::new());
 }
