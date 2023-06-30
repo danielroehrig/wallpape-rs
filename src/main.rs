46dd8454ec19f849a331035aacc476ea56e59f8e
Daniel Röhrig <mail@danielroehrig.de>
2023-06-30 22:56:56 +0200

    Start splitting up the code

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::collections::HashMap;
 use std::process::exit;
 use voronator::delaunator::Point as Vpoint;
 use voronator::VoronoiDiagram;
+use wallpape_rs::palettes::build_palettes;
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -91,41 +92,6 @@ fn print_palettes(palettes: &HashMap<&str, Vec<Color>>) {
     for scheme in palettes.keys() {
         println!("{}", scheme);
     }
-}
-
-fn build_palettes() -> HashMap<&'static str, Vec<Color>> {
-    HashMap::from([
-        (
-            "cyberpunk",
-            vec![
-                Color::new(0xff, 247, 37, 133),
-                Color::new(0xff, 114, 9, 183),
-                Color::new(0xff, 58, 12, 163),
-                Color::new(0xff, 67, 97, 238),
-                Color::new(0xff, 76, 201, 240),
-            ],
-        ),
-        (
-            "pastel",
-            vec![
-                Color::new(0xff, 205, 180, 219),
-                Color::new(0xff, 255, 200, 221),
-                Color::new(0xff, 255, 175, 204),
-                Color::new(0xff, 189, 224, 254),
-                Color::new(0xff, 162, 210, 255),
-            ],
-        ),
-        (
-            "gundam",
-            vec![
-                Color::new(0xff, 43, 45, 66),
-                Color::new(0xff, 141, 153, 174),
-                Color::new(0xff, 237, 242, 244),
-                Color::new(0xff, 239, 35, 60),
-                Color::new(0xff, 217, 4, 41),
-            ],
-        ),
-    ])
 }
 
 fn draw_gradient(palette: &Vec<Color>, dt: &mut DrawTarget) {
