76ead43b694ef4630d0bc6d48794d6437a9c4782
Daniel Röhrig <mail@danielroehrig.de>
2023-08-25 21:42:44 +0200

    Put all effects into the library

--- src/effects.rs
+++ src/effects.rs
@@ -1,8 +1,14 @@
+use rand::distributions::Uniform;
 use rand::seq::SliceRandom;
-use rand::RngCore;
-use raqote::{Color, DrawOptions, DrawTarget, PathBuilder, SolidSource, Source};
+use rand::{Rng, RngCore};
+use raqote::{
+    Color, DrawOptions, DrawTarget, Gradient, GradientStop, PathBuilder, Point, SolidSource,
+    Source, Spread, StrokeStyle,
+};
 use std::cmp::{max, min};
 use std::process::exit;
+use voronator::delaunator::Point as Vpoint;
+use voronator::VoronoiDiagram;
 
 pub fn draw_little_boxes(palette: &Vec<Color>, dt: &mut DrawTarget) {
     let rng_color: Color;
@@ -39,5 +45,84 @@ pub fn draw_little_boxes(palette: &Vec<Color>, dt: &mut DrawTarget) {
             let path = pb.finish();
             dt.fill(&path, &color, &DrawOptions::new());
         }
+    }
+}
+
+pub fn draw_gradient(palette: &Vec<Color>, dt: &mut DrawTarget) {
+    let colors: Vec<&Color> = palette
+        .choose_multiple(&mut rand::thread_rng(), 2)
+        .collect();
+
+    if colors.len() < 2 {
+        eprintln!("Too few colors for gradient effect");
+        exit(1);
+    }
+
+    let mut pb = PathBuilder::new();
+    pb.rect(0., 0., dt.width() as f32, dt.height() as f32);
+    let path = pb.finish();
+
+    let gradient = Source::new_linear_gradient(
+        Gradient {
+            stops: vec![
+                GradientStop {
+                    position: 0.0,
+                    color: **(colors.get(0).expect("Missing Color")),
+                },
+                GradientStop {
+                    position: 1.0,
+                    color: **(colors.get(1).expect("Missing Color")),
+                },
+            ],
+        },
+        Point::new(0., 0.),
+        Point::new(dt.width() as f32, dt.height() as f32),
+        Spread::Pad,
+    );
+    dt.fill(&path, &gradient, &DrawOptions::new());
+}
+
+pub fn draw_voronoi(palette: &Vec<Color>, dt: &mut DrawTarget) {
+    let rng = &mut rand::thread_rng();
+    let range1 = Uniform::new(0., dt.width() as f64);
+    let range2 = Uniform::new(0., dt.height() as f64);
+    let points: Vec<(f64, f64)> = (0..100)
+        .map(|_| (rng.sample(&range1), rng.sample(&range2)))
+        .collect();
+    let diagram = VoronoiDiagram::<Vpoint>::from_tuple(
+        &(0., 0.),
+        &(dt.width() as f64, dt.height() as f64),
+        &points,
+    )
+    .unwrap();
+    let cells = diagram.cells();
+    for c in cells {
+        let rng_color: Color;
+        match palette.choose(rng) {
+            Some(c) => rng_color = *c,
+            None => {
+                eprintln!("Palette seems to be empty");
+                exit(1);
+            }
+        }
+        let mut pb = PathBuilder::new();
+        let fp = c.points().first().unwrap();
+        pb.move_to(fp.x as f32, fp.y as f32);
+        for p in c.points() {
+            pb.line_to(p.x as f32, p.y as f32)
+        }
+        pb.line_to(fp.x as f32, fp.y as f32);
+        let path = pb.finish();
+        dt.stroke(
+            &path,
+            &Source::Solid(SolidSource::from(Color::new(0xff, 0x00, 0x00, 0x00))),
+            &StrokeStyle::default(),
+            &DrawOptions::new(),
+        );
+        dt.fill(
+            &path,
+            &Source::Solid(SolidSource::from(rng_color)),
+            &DrawOptions::new(),
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use clap::{Parser, Subcommand};
-use rand::distributions::Uniform;
-use rand::seq::SliceRandom;
-use rand::Rng;
 use raqote::*;
 use std::process::exit;
-use voronator::delaunator::Point as Vpoint;
-use voronator::VoronoiDiagram;
 use wallpape_rs::effects::*;
 use wallpape_rs::palettes::*;
 
@@ -69,8 +64,8 @@ fn main() {
     let mut dt = DrawTarget::new(width, height);
 
     // draw_gradient(palette, &mut dt);
-    draw_little_boxes(palette, &mut dt);
-    // draw_voronoi(palette, &mut dt);
+    // draw_little_boxes(palette, &mut dt);
+    draw_voronoi(palette, &mut dt);
 
     match dt.write_png(dest.clone()) {
         Ok(_) => {
@@ -88,84 +83,5 @@ fn print_palettes() {
     println!("Available colorschemes:");
     for scheme in get_palette_names() {
         println!("{}", scheme);
-    }
-}
-
-fn draw_gradient(palette: &Vec<Color>, dt: &mut DrawTarget) {
-    let colors: Vec<&Color> = palette
-        .choose_multiple(&mut rand::thread_rng(), 2)
-        .collect();
-
-    if colors.len() < 2 {
-        eprintln!("Too few colors for gradient effect");
-        exit(1);
-    }
-
-    let mut pb = PathBuilder::new();
-    pb.rect(0., 0., dt.width() as f32, dt.height() as f32);
-    let path = pb.finish();
-
-    let gradient = Source::new_linear_gradient(
-        Gradient {
-            stops: vec![
-                GradientStop {
-                    position: 0.0,
-                    color: **(colors.get(0).expect("Missing Color")),
-                },
-                GradientStop {
-                    position: 1.0,
-                    color: **(colors.get(1).expect("Missing Color")),
-                },
-            ],
-        },
-        Point::new(0., 0.),
-        Point::new(dt.width() as f32, dt.height() as f32),
-        Spread::Pad,
-    );
-    dt.fill(&path, &gradient, &DrawOptions::new());
-}
-
-fn draw_voronoi(palette: &Vec<Color>, dt: &mut DrawTarget) {
-    let rng = &mut rand::thread_rng();
-    let range1 = Uniform::new(0., dt.width() as f64);
-    let range2 = Uniform::new(0., dt.height() as f64);
-    let points: Vec<(f64, f64)> = (0..100)
-        .map(|_| (rng.sample(&range1), rng.sample(&range2)))
-        .collect();
-    let diagram = VoronoiDiagram::<Vpoint>::from_tuple(
-        &(0., 0.),
-        &(dt.width() as f64, dt.height() as f64),
-        &points,
-    )
-    .unwrap();
-    let cells = diagram.cells();
-    for c in cells {
-        let rng_color: Color;
-        match palette.choose(rng) {
-            Some(c) => rng_color = *c,
-            None => {
-                eprintln!("Palette seems to be empty");
-                exit(1);
-            }
-        }
-        let mut pb = PathBuilder::new();
-        let fp = c.points().first().unwrap();
-        pb.move_to(fp.x as f32, fp.y as f32);
-        for p in c.points() {
-            pb.line_to(p.x as f32, p.y as f32)
-        }
-        pb.line_to(fp.x as f32, fp.y as f32);
-        let path = pb.finish();
-        dt.stroke(
-            &path,
-            &Source::Solid(SolidSource::from(Color::new(0xff, 0x00, 0x00, 0x00))),
-            &StrokeStyle::default(),
-            &DrawOptions::new(),
-        );
-        dt.fill(
-            &path,
-            &Source::Solid(SolidSource::from(rng_color)),
-            &DrawOptions::new(),
-        );
     }
 }
